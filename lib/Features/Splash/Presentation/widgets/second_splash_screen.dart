02d97712f4f0406561f7ee95965d9455963a3dae
Khaledelsayed10 <llolococo123@gmail.com>
2025-02-18 20:25:10 +0200

    finish splash

--- lib/Features/Splash/Presentation/widgets/second_splash_screen.dart
+++ lib/Features/Splash/Presentation/widgets/second_splash_screen.dart
@@ -6,37 +6,20 @@ import 'package:sehatak/core/widget/background_image.dart';
 
 import '../../../on Boarding/Presentation/on_boarding_view.dart';
 
-class SecondSplashScreen extends StatefulWidget {
+class SecondSplashScreen extends StatelessWidget {
   const SecondSplashScreen({super.key});
 
   @override
-  _SecondSplashScreenState createState() => _SecondSplashScreenState();
-}
-
-class _SecondSplashScreenState extends State<SecondSplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-    _animation = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
-    _controller.forward();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      goToNextView();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const OnBoardingView(),
+        transition: Transition.fade,
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOut,
+      );
+    });
+
     return Scaffold(
       body: Stack(
         children: [
@@ -54,20 +37,23 @@ class _SecondSplashScreenState extends State<SecondSplashScreen>
                   ),
                 ),
                 SizedBox(height: 10.h),
-                ScaleTransition(
-                  scale: _animation,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Nezamk',
-                        style: TextStyle(
-                          fontSize: 45.sp,
-                          fontWeight: FontWeight.bold,
-                          color: accentColor,
-                        ),
-                      ),
-                    ],
+                TweenAnimationBuilder<double>(
+                  tween: Tween(begin: 0.0, end: 1.0),
+                  duration: const Duration(seconds: 2),
+                  curve: Curves.easeInOut,
+                  builder: (context, value, child) {
+                    return Transform.scale(
+                      scale: value,
+                      child: child,
+                    );
+                  },
+                  child: Text(
+                    'Nezamk',
+                    style: TextStyle(
+                      fontSize: 45.sp,
+                      fontWeight: FontWeight.bold,
+                      color: accentColor,
+                    ),
                   ),
                 ),
               ],
@@ -77,32 +63,4 @@ class _SecondSplashScreenState extends State<SecondSplashScreen>
       ),
     );
   }
-
-  void goToNextView() {
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.to(
-        () => const OnBoardingView(),
-        transition: Transition.fade,
-        duration: const Duration(seconds: 1),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-}
-
-void goToNextView() {
-  Future.delayed(const Duration(seconds: 2), () {
-    Get.to(
-      () => const OnBoardingView(),
-      transition: Transition.fade,
-      duration: const Duration(seconds: 1),
-      curve: Curves.easeInOut,
-    );
-  });
 }
